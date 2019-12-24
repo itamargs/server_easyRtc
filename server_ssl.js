293d90eb860cd4565a2bb8c43a7ef1224dc8b7fa
itamargs <itamargs@gmail.com>
2019-12-24 17:40:45 +0200

    fix.

--- server_ssl.js
+++ server_ssl.js
@@ -3,7 +3,7 @@ var https   = require("https");     // https server core module
 var fs      = require("fs");        // file system core module
 var express = require("express");   // web framework external module
 var io      = require("socket.io"); // web socket external module
-var cors = require('cors');
+// var cors = require('cors');
 
 // This sample is using the easyrtc from parent folder.
 // To use this server_example folder only without parent folder:
@@ -16,12 +16,26 @@ var easyrtc = require("easyrtc");; // EasyRTC internal module
 var httpApp = express();
 httpApp.use(express.static(__dirname + "/static/"));
 
+// Add headers
+httpApp.use(function (req, res, next) {
 
-httpApp.use(cors())
- 
-httpApp.get('/products/:id', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
-})
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 
 // Start Express https server on port 8443
 var webServer = https.createServer({
