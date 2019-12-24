a8abde3ec5181fe86b549cf60094b21c6127ac19
itamargs <itamargs@gmail.com>
2019-12-24 17:46:34 +0200

    fix.

--- server_ssl.js
+++ server_ssl.js
@@ -50,7 +50,7 @@ var socketServer = io.listen(webServer, {"log level":1});
 var rtc = easyrtc.listen(httpApp, socketServer);
 
 // Listen on port 8443
-webServer.listen(443, "0.0.0.0", function () {
+webServer.listen(443, function () {
     var port = webServer.address().port;
     console.log('listening on https://localhost in port ' + port);
 });
