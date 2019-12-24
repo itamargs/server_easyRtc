837a92b7983824855de75a8611c4b7ceb3ca4569
itamargs <itamargs@gmail.com>
2019-12-24 21:44:27 +0200

    fix.

--- server.js
+++ server.js
@@ -26,27 +26,34 @@ var socketServer = socketIo.listen(webServer, {"log level":1});
 
 easyrtc.setOption("logLevel", "debug");
 
-var myIceServers = [
-    {"urls":"stun:stun:numb.viagenie.ca:443"},
-    {
-      "urls":"turn:stun:numb.viagenie.ca:443", 
-      "username":"itamargs111@gmail.com",
-      "credential":"igwebrctpass"
-    },
-    {
-        "urls":"turn:stun:numb.viagenie.ca?:443transport=tcp", 
-        "username":"itamargs111@gmail.com",
-        "credential":"igwebrctpass"
-    }
-  ];
+var appIceServers = [                                    // Array of STUN and TURN servers. By default there is only publicly available STUN servers.
+    {urls: "stun:stun.l.google.com:19302"},
+    {urls: "stun:stun.sipgate.net"},
+    {urls: "stun:217.10.68.152"},
+    {urls: "stun:stun.sipgate.net:10000"},
+    {urls: "stun:217.10.68.152:10000"}
+];
+
+easyrtc.setOption("appIceServers", appIceServers);
 
 
-  var check = easyrtc.setOption("appIceServers", myIceServers);
+// var myIceServers = [
+//     {"urls":"stun:stun:numb.viagenie.ca:443"},
+//     {
+//       "urls":"turn:stun:numb.viagenie.ca:443", 
+//       "username":"itamargs111@gmail.com",
+//       "credential":"igwebrctpass"
+//     },
+//     {
+//         "urls":"turn:stun:numb.viagenie.ca?:443transport=tcp", 
+//         "username":"itamargs111@gmail.com",
+//         "credential":"igwebrctpass"
+//     }
+//   ];
 
 
-//   easyrtc.on("getIceConfig", function(connectionObj, callback){
-//     callback(null, myIceServers);
-//   })
+
+
 
 // Overriding the default easyrtcAuth listener, only so we can directly access its callback
 easyrtc.events.on("easyrtcAuth", function(socket, easyrtcid, msg, socketCallback, callback) {
